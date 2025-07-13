a2fd1af64c7bfac59b842e4f35ec993a84fa5f90
liakh83 <lahn4573@gmail.com>
2025-07-13 11:24:14 +0300

    feat: add controlled Header with input and search button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,24 @@
 import { Component, type ReactNode } from 'react';
 
-export default class Header extends Component {
+interface Props {
+  inputValue: string;
+  onSearchInput: (value: string) => void;
+  onSearchClick: () => void;
+}
+
+export default class Header extends Component<Props> {
   render(): ReactNode {
+    const { inputValue, onSearchInput, onSearchClick } = this.props;
     return (
       <header className="flex item-center gap-4 p-4">
         <input
+          value={inputValue}
+          onChange={(e) => onSearchInput(e.target.value)}
           placeholder="input name pokemon"
           type="text"
           className="border rounded px-4 py-2 focus:outline-none focus:ring-2"
         ></input>
-        <button>search</button>
+        <button onClick={onSearchClick}>search</button>
       </header>
     );
   }
