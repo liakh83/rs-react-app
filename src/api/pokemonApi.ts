4001077b1ae841ea0640692a0ef26c06b582c700
liakh83 <lahn4573@gmail.com>
2025-07-13 11:31:12 +0300

    feat: add fetchPokemonByName function for searching Pokemon by name

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -1,5 +1,14 @@
 export const fetchPokemonList = async () => {
   const result = await fetch('https://pokeapi.co/api/v2/pokemon');
-  if (!result.ok) throw new Error(`Error ${result.status}`);
+  if (!result.ok) throw new Error(`Error status: ${result.status}`);
+  return result.json();
+};
+
+export const fetchPokemonByName = async (name: string) => {
+  const result = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase().trim()}`
+  );
+  if (!result.ok)
+    throw new Error(`Pokemon "${name}" not faund . Status: ${result.status}`);
   return result.json();
 };
