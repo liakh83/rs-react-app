a895d0fb155c65486007a2188152c44c92790e02
liakh83 <lahn4573@gmail.com>
2025-07-14 03:04:05 +0300

    refactor: remove any usage and apply strong typings to API responses

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -1,14 +1,27 @@
-export const fetchPokemonList = async () => {
+import type {
+  PokemonAPIResponse,
+  PokemonListResponse,
+} from '../utils/interfaces';
+
+export const fetchPokemonList = async (): Promise<PokemonListResponse> => {
   const result = await fetch('https://pokeapi.co/api/v2/pokemon');
   if (!result.ok) throw new Error(`Error status: ${result.status}`);
   return result.json();
 };
 
 export const fetchPokemonByName = async (name: string) => {
-  const result = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase().trim()}`
-  );
+  const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   if (!result.ok)
-    throw new Error(`Pokemon "${name}" not faund . Status: ${result.status}`);
-  return result.json();
+    throw new Error(`Pokemon "${name}" not found . Status: ${result.status}`);
+  const data: PokemonAPIResponse = await result.json();
+
+  return {
+    id: data.id,
+    name: data.name,
+    image: data.sprites.front_default,
+    types: data.types.map((value) => value.type.name),
+    abilities: data.abilities.map((value) => value.ability.name),
+    height: data.height / 10,
+    weight: data.weight / 10,
+  };
 };
