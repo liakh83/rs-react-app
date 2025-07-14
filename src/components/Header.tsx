1c32c6f417af86bbe3a7d7836afa2913e47663c9
liakh83 <lahn4573@gmail.com>
2025-07-14 03:06:42 +0300

    style: improve layout and visual appearance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default class Header extends Component<Props> {
   render(): ReactNode {
     const { inputValue, onSearchInput, onSearchClick } = this.props;
     return (
-      <header className="flex item-center gap-4 p-4">
+      <header className="flex items-center justify-center gap-4 p-4">
         <input
           value={inputValue}
           onChange={(e) => onSearchInput(e.target.value)}
