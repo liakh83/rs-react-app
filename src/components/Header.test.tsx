c62fe28f68db7d8b11a0af29787e374601a5702c
liakh83 <lahn4573@gmail.com>
2025-07-19 21:06:14 +0300

    test: add unit tests for input and search button behavior

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,50 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Header from './Header';
+
+describe('Search component tests', () => {
+  it('renders search input and search button', () => {
+    render(
+      <Header inputValue="" onSearchInput={() => {}} onSearchClick={() => {}} />
+    );
+
+    expect(
+      screen.getByPlaceholderText(/input name pokemon/i)
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /search/i })).toBeInTheDocument();
+  });
+
+  it('calls onSearchInput when taping in input', async () => {
+    const mockOnSearchInput = vi.fn();
+
+    render(
+      <Header
+        inputValue=""
+        onSearchInput={mockOnSearchInput}
+        onSearchClick={() => {}}
+      />
+    );
+
+    const input = screen.getByPlaceholderText(/input name pokemon/i);
+    await userEvent.type(input, 'pikachu');
+
+    expect(mockOnSearchInput).toBeCalledTimes(7);
+  });
+
+  it('calls onSearchClick when button is clicked', async () => {
+    const mockOnSearchClick = vi.fn();
+
+    render(
+      <Header
+        inputValue=""
+        onSearchInput={() => {}}
+        onSearchClick={mockOnSearchClick}
+      />
+    );
+
+    const button = screen.getByRole('button', { name: /search/i });
+    await userEvent.click(button);
+    expect(mockOnSearchClick).toHaveBeenCalled();
+  });
+});
