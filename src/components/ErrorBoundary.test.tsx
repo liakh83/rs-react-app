8338ebd633940db4ebae4a9e0a050a59b752c27e
liakh83 <lahn4573@gmail.com>
2025-07-28 01:53:14 +0300

    test: move ErrorBoundary tests to a separate file for better organization

---
+++ src/components/ErrorBoundary.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
+import ErrorBoundary from './ErrorBoundary';
+
+import type { JSX } from 'react';
+
+function ProblemChild(): JSX.Element {
+  throw new Error('Error thrown from problem child');
+}
+
+describe('ErrorBoundary', () => {
+  it('renders fallback UI on error', async () => {
+    const errorSpy = vi.spyOn(console, 'error').mockImplementation(() => {});
+    render(
+      <ErrorBoundary>
+        <ProblemChild />
+      </ErrorBoundary>
+    );
+
+    const fallbackHeading = await screen.findByText(
+      /Artificial error for testing the ErrorBoundary!/i
+    );
+
+    expect(fallbackHeading).toBeInTheDocument();
+    errorSpy.mockRestore();
+  });
+});
