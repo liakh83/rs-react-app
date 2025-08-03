4856785cfa3faf9bde9387b85e4f82a6330c3089
liakh83 <lahn4573@gmail.com>
2025-08-04 01:19:56 +0300

    feat: add selectedItemsSlice and wrap app with Redux Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import { ThemeProvider } from '@components/context';
+import { store } from '@redux/store';
 import './index.css';
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/redux/selectedItemsSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+import type { Pokemon } from '@utils/interfaces';
+
+interface SelectedItemsState {
+  selectedItems: Record<string, Pokemon>;
+}
+
+const initialState: SelectedItemsState = {
+  selectedItems: {},
+};
+
+const selectedItemsSlice = createSlice({
+  name: 'selectedItems',
+  initialState,
+  reducers: {
+    toggleItem: (state, action: PayloadAction<Pokemon>) => {
+      const name = action.payload.name;
+      if (state.selectedItems[name]) {
+        state.selectedItems = Object.fromEntries(
+          Object.entries(state.selectedItems).filter(([key]) => key !== name)
+        );
+      } else {
+        state.selectedItems[name] = action.payload;
+      }
+    },
+    clearItems: (state) => {
+      state.selectedItems = {};
+    },
+  },
+});
+
+export const { toggleItem, clearItems } = selectedItemsSlice.actions;
+
+export default selectedItemsSlice.reducer;
