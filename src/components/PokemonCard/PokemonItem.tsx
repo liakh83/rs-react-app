8369e27e31e492e5680f5a17325f96524980484c
liakh83 <lahn4573@gmail.com>
2025-08-04 01:25:37 +0300

    feat: add saving of selected pokemons to the store

--- src/components/PokemonCard/PokemonItem.tsx
+++ src/components/PokemonCard/PokemonItem.tsx
@@ -1,5 +1,6 @@
 import Loader from '@components/Loader';
-import { usePokemonByName } from '@hooks/index';
+import { useAppDispatch, useAppSelector, usePokemonByName } from '@hooks/index';
+import { toggleItem } from '@redux/selectedItemsSlice';
 
 import PokemonCard from './PokemonCard';
 
@@ -14,12 +15,29 @@ const PokemonItem = ({
 }) => {
   const { pokemon, isLoading, error } = usePokemonByName(name);
 
+  const dispatch = useAppDispatch();
+  const isSelected = useAppSelector(
+    (state) => !!state.selectedItems.selectedItems[name]
+  );
+
+  const handleCheckboxChange = () => {
+    if (pokemon) {
+      dispatch(toggleItem(pokemon));
+    }
+  };
+
   if (isLoading) return <Loader />;
   if (error || !pokemon) return null;
   return (
-    <>
+    <div className="relative">
+      <input
+        type="checkbox"
+        checked={isSelected}
+        onChange={handleCheckboxChange}
+        className="absolute top-2 right-2 z-10 w-5 h-5"
+      />
       <PokemonCard pokemon={pokemon} onClick={() => onClick?.(pokemon)} />
-    </>
+    </div>
   );
 };
 
