766503ca1df873a3ed36caf0cf0a6701a6af5569
liakh83 <lahn4573@gmail.com>
2025-08-10 20:36:57 +0300

    feat: add logic and buttons for cache invalidation and refetching

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Outlet, useSearchParams } from 'react-router-dom';
 
+import { pokemonApi } from '@api/pokemonApi';
 import CardList from '@components/CardList';
 import FlyoutElement from '@components/FlyoutElement';
 import Header from '@components/Header';
 import Pagination from '@components/Pagination';
 import { PokemonItem } from '@components/PokemonCard';
-import { useLocalStorageState } from '@hooks/index';
+import { useAppDispatch, useLocalStorageState } from '@hooks/index';
+import { useAppSelector } from '@hooks/index';
+import { LIMIT_ITEMS } from '@utils/constants';
 
 import type { Pokemon } from '@utils/interfaces';
-
-const LIMIT = 12;
 
 const MainPage = () => {
   const [inputValue, setInputValue] = useLocalStorageState('inputValue', '');
@@ -18,9 +19,20 @@ const MainPage = () => {
   const [totalCount, setTotalCount] = useState(0);
   const [shouldThrow, setShouldThrow] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
+  const dispatch = useAppDispatch();
 
   const currentPage = Number(searchParams.get('page')) || 1;
-  const totalPage = Math.ceil(totalCount / LIMIT);
+  const detailsPokemonName = searchParams.get('details');
+  const searchPokemonName = searchParams.get('search');
+  const totalPage = Math.ceil(totalCount / LIMIT_ITEMS);
+  const limit = LIMIT_ITEMS;
+  const offset = (currentPage - 1) * limit;
+
+  const selectPokemonList = pokemonApi.endpoints.getPokemonList.select({
+    offset,
+    limit,
+  });
+  const selectPokemonResult = useAppSelector(selectPokemonList);
 
   useEffect(() => {
     const hasSearch = searchParams.get('search');
@@ -45,7 +57,7 @@ const MainPage = () => {
     }
   }, [searchTerm, setSearchParams, searchParams]);
 
-  const handleSearchInput = (value: string) => {
+  const handleChangeSearchInputValue = (value: string) => {
     setInputValue(value);
   };
 
@@ -84,6 +96,53 @@ const MainPage = () => {
     }
   };
 
+  const handleResetAllCache = () => {
+    dispatch(pokemonApi.util.resetApiState());
+  };
+
+  const handleRefetch = () => {
+    if (searchPokemonName) {
+      dispatch(
+        pokemonApi.endpoints.getPokemonByName.initiate(searchPokemonName, {
+          forceRefetch: true,
+        })
+      );
+
+      if (detailsPokemonName) {
+        dispatch(
+          pokemonApi.endpoints.getPokemonByName.initiate(detailsPokemonName, {
+            forceRefetch: true,
+          })
+        );
+      }
+      return;
+    }
+
+    dispatch(
+      pokemonApi.endpoints.getPokemonList.initiate(
+        { offset, limit },
+        { forceRefetch: true }
+      )
+    );
+
+    const pokemonNames = selectPokemonResult.data?.pokemonNames;
+    pokemonNames?.forEach((pokemonName) => {
+      dispatch(
+        pokemonApi.endpoints.getPokemonByName.initiate(pokemonName, {
+          forceRefetch: true,
+        })
+      );
+    });
+
+    if (detailsPokemonName) {
+      dispatch(
+        pokemonApi.endpoints.getPokemonByName.initiate(detailsPokemonName, {
+          forceRefetch: true,
+        })
+      );
+    }
+  };
+
   const handleError = () => {
     setShouldThrow(true);
   };
@@ -96,16 +155,27 @@ const MainPage = () => {
     <>
       <Header
         inputValue={inputValue}
-        onSearchInput={handleSearchInput}
+        onSearchInput={handleChangeSearchInputValue}
         onSearchClick={handleSearch}
       />
+      <div className=" p-4 ">
+        <button className="mx-2" onClick={handleRefetch}>
+          Refresh
+        </button>
+        <button
+          className="mx-2 bg-red-600 text-white hover: bg-red-700 transition"
+          onClick={handleResetAllCache}
+        >
+          Reset all cache
+        </button>
+      </div>
       <div className="flex">
-        <div className="flex-1 card" onClick={handleCloseDetail}>
+        <div className="flex-1" onClick={handleCloseDetail}>
           {!searchTerm && (
             <>
               <CardList
                 page={currentPage}
-                limit={LIMIT}
+                limit={LIMIT_ITEMS}
                 onTotalCountChange={setTotalCount}
                 onPokemonClick={handlePokemonClick}
               />
